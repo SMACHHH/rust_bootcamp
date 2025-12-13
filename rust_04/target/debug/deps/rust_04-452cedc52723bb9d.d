c8da47bc9d16c2154ff52ba85fc293b6c95cec89
SMACHHH <163435469+SMACHHH@users.noreply.github.com>
2025-12-13 18:01:48 +0000

    Format and lint rust projects

---
+++ rust_04/target/debug/deps/rust_04-452cedc52723bb9d.d
@@ -0,0 +1,9 @@
+/workspaces/rust_bootcamp/rust_04/target/debug/deps/rust_04-452cedc52723bb9d.d: src/main.rs Cargo.toml
+
+/workspaces/rust_bootcamp/rust_04/target/debug/deps/librust_04-452cedc52723bb9d.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ rust_04/target/debug/rust_04.d
@@ -0,0 +1,1 @@
+/workspaces/rust_bootcamp/rust_04/target/debug/rust_04: /workspaces/rust_bootcamp/rust_04/src/main.rs
